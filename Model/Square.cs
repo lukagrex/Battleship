76ac6461ce2762de115ce04f4f18dea8588ffa0e
Robert Haramincic <RHaramincic@dspace.hr>
2022-03-14 20:37:20 +0100

    Created Model Project and Square Class

---
+++ Model/Square.cs
@@ -0,0 +1,15 @@
+﻿namespace Model
+{
+    public class Square
+    {
+        public readonly int Row;
+        public readonly int Column;
+
+
+        public Square(int row, int column)
+        {
+            this.Row = row;
+            this.Column = column;
+        }
+    }
+}
---
+++ UnitTests/TestSquare.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestSquare
+    {
+        [TestMethod]
+        public void ConstructorCreatesSquareWithGivenCoordinates()
+        {
+            Square s = new Square(1, 5);
+            Assert.AreEqual(1, s.Row);
+            Assert.AreEqual(5, s.Column);
+        }
+    }
+}
