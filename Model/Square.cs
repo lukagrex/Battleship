75b161463f88ec70938b16a13dc4b0143d240050
Pavle Pavicic <ppavicic@dspace.hr>
2022-03-14 20:34:52 +0100

    Created Model and UnitTests projects, Square class and corresponding Unit Test class.

---
+++ Model/Square.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Square
+    {
+        public Square(int row, int column)
+        {
+            this.Row = row;
+            this.Column = column;
+        }
+
+        public readonly int Row;
+        public readonly int Column;
+    }
+}
---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("dSPACE GmbH")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright © dSPACE GmbH 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7b826902-e69d-4a1c-8526-c3c57d398bc1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
