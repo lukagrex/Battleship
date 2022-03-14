a274db07cdb9a775b185fb06ad034153a58ab911
Andrea Vrljic <AVrljic@dspace.hr>
2022-03-14 20:34:47 +0100

    Create Model project and  Square class

---
+++ Model/Square.cs
@@ -0,0 +1,15 @@
+﻿namespace Model
+{
+    public class Square
+    {
+        public readonly int Row;
+        public readonly int Column;
+
+
+        public Square(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+    }
+}
