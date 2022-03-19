5f018e170c9f36b57f3884533e6d6ea65bbb1eef
EugenPoljicak <epoljicak@dspace.hr>
2022-03-19 21:12:00 +0100

    Create Square class and unit test for it

---
+++ Model/Square.cs
@@ -0,0 +1,14 @@
+﻿namespace Model
+{
+    public class Square
+    {
+        public Square(int row, int column)
+        {
+            this.Row = row;
+            this.Column = column;
+        }
+
+        public readonly int Row;
+        public readonly int Column;
+    }
+}
---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("dSPACE GmbH")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright © dSPACE GmbH 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("82727024-acc0-4ba2-a283-316d74dda987")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
