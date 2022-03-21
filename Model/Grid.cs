ec29aedbbcd4422dba44bcea0681435223784d7c
AndjelaJ <AJuric@dspace.hr>
2022-03-21 19:44:11 +0100

    Create Grid class

---
+++ Model/Grid.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+
+namespace Vsite.Battleship.Model
+{
+    using SquareSequence = IEnumerable<Square>;
+
+    public class Grid
+    {
+        public Grid(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            squares = new Square[Rows, Columns];
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Columns; c++)
+                {
+                    squares[r, c] = new Square(r, c);
+                }
+            }
+        }
+
+        public IEnumerable<Square> Squares
+        {
+            get { return squares.Cast<Square>().Where(s => s != null); }
+        }
+
+        public IEnumerable<SquareSequence> GetAvailablePlacements(int length)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public readonly int Rows;
+        public readonly int Columns;
+
+        private Square[,] squares;
+    }
+}
