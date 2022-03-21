45989ae499a6ab01bc157d1b647997fc4a389d35
Vicko Cagalj <vickocagalj@gmail.com>
2022-03-21 22:58:34 +0100

    Create Grid class

---
+++ UnitTests/TestGrid.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using Vsite.Battleship.Model;
+
+
+namespace Vsite.Battleship
+{
+    [TestClass]
+    public class TestGrid
+    {
+        [TestMethod]
+        public void ConstructorCreatesGridOf100SquaresForAGridWith10Rows10Columns()
+        {
+            Grid grid = new Grid(10, 10);
+            Assert.AreEqual(100, grid.Squares.Count());
+            Assert.IsTrue(grid.Squares.Contains(new Square(0, 0)));
+            Assert.IsTrue(grid.Squares.Contains(new Square(9, 9)));
+            Assert.IsTrue(grid.Squares.Contains(new Square(0, 9)));
+            Assert.IsTrue(grid.Squares.Contains(new Square(9, 0)));
+        }
+
+        [TestMethod]
+        public void GetAvailablePlacementsReturns2PlacementsForAShip3SquaresLongOnGrid1Row4Columns()
+        {
+            Grid grid = new Grid(1, 4);
+            var placements = grid.GetAvailablePlacements(3);
+            Assert.AreEqual(2, placements.Count());
+        }
+
+        [TestMethod]
+        public void GetAvailablePlacementsReturns2PlacementsForAShip3SquaresLongOnGrid5Rows1Column()
+        {
+            Grid grid = new Grid(5, 1);
+            var placements = grid.GetAvailablePlacements(3);
+            Assert.AreEqual(3, placements.Count());
+        }
+    }
+}
