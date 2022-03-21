746156ae85ea1f8f7659e65bb1edb9c63358bb34
Kata Pogacic <KPogacic@dspace.hr>
2022-03-21 19:44:10 +0100

    Create Grid Class

--- Model/Square.cs
+++ Model/Square.cs
@@ -1,6 +1,8 @@
-﻿namespace Vsite.BattleShip.Model
+﻿using System;
+
+namespace Vsite.BattleShip.Model
 {
-    public class Square
+    public class Square : IEquatable<Square>
     {
         public Square(int row, int column)
         {
@@ -10,5 +12,28 @@
 
         public readonly int Row;
         public readonly int Column;
+        public bool Equals(Square other)
+        {
+            return Row == other.Row && Column == other.Column;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (GetType() != obj.GetType())
+                return false;
+            return Equals((Square)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 240067226;
+
+            hashCode = hashCode * -1521134295 + Row.GetHashCode();
+            hashCode = hashCode * -1521134295 + Column.GetHashCode();
+
+            return hashCode;
+        }
     }
 }
---
+++ UnitTests/TestGrid.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+namespace Vsite.BattleShip
+{
+    [TestClass]
+    public class TestGrid
+    {
+        [TestMethod]
+        public void ConstructorCreatesGridOf100SquaresForAGridWith10Rows10Columns()
+        {
+            Grid grid = new Grid(10, 10);
+            Assert.AreEqual(100, grid.Squares.Count());
+            Assert.IsTrue(grid.Squares.Contains(new Square(0, 0)));
+            Assert.IsTrue(grid.Squares.Contains(new Square(9, 9)));
+            Assert.IsTrue(grid.Squares.Contains(new Square(0, 9)));
+            Assert.IsTrue(grid.Squares.Contains(new Square(9, 0)));
+        }
+        [TestMethod]
+        public void GetAvailablePlacementReturnsForAShip3SquaresLongOnGrid1Rows4Columns()
+        {
+            Grid grid = new Grid(1, 4);
+            var placements = grid.GetAvailablePlacements(3);
+            Assert.AreEqual(2, placements.Count());
+        }
+        [TestMethod]
+        public void GetAvailablePlacementReturnsForAShip3SquaresLongOnGrid1Rows5Columns()
+        {
+            Grid grid = new Grid(5, 1);
+            var placements = grid.GetAvailablePlacements(3);
+            Assert.AreEqual(2, placements.Count());
+        }
+
+    }
+}
