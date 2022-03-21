a7a9f67d224391f8ec206392573ae15d8acb8dbe
Ivan Sokolović <isokolovic1@gmail.com>
2022-03-21 19:44:06 +0100

    Create Grid class - to be corrected

---
+++ Model/Grid.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Battleship.Model
+{
+    using SquareSequence = IEnumerable<Square>;
+
+    public class Grid
+    {
+        public Grid(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            squares = new Square[Rows, Columns];
+
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++)
+                {
+                    squares[i, j] = new Square(i, j);
+                }
+            }
+        }
+
+        public IEnumerable<Square> squares 
+        {
+            get
+            {
+                squares.Cast<Square>().Where(s => s != null);
+            }
+        }
+
+        public IEnumerable<Square> GetAvailablePlacements(int length)
+        {
+            throw new NotImplementedException();
+        }
+
+        public readonly int Rows;
+        public readonly int Columns;
+
+        private Square[,] squares;
+    }
+}
--- Model/Square.cs
+++ Model/Square.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Vsite.Battleship.Model
 {
-    public class Square
+    public class Square : IEquatable<Square>
     {
         public Square(int row, int column)
         {
@@ -16,5 +16,31 @@ namespace Vsite.Battleship.Model
 
         public readonly int Row;
         public readonly int Column;
+
+        public bool Equals(Square other)
+        {
+            return other != null && Row == other.Row && Column == other.Column;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(obj == null)
+            {
+                return false;
+            }
+            if(obj.GetType() != obj.GetType())
+            {
+                return false;
+            }
+            return Equals((Square)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 240067226;
+            hashCode = hashCode * -1521134295 + Row.GetHashCode();
+            hashCode = hashCode * -1521134295 + Column.GetHashCode();
+            return hashCode;
+        }
     }
 }
