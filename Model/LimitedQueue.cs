df10cd1adfd1a1c41c5a104cb19d990ab0de757e
Ivan Spanja <IvanSp@dspace.hr>
2022-03-21 20:24:02 +0100

    Added queue functions

---
+++ Model/LimitedQueue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Battleship.Model
+{
+    public class LimitedQueue<T> : Queue<T>
+    {
+        public LimitedQueue(int length)
+        {
+            this.length = length;
+        }
+
+        public void Enqueue(T item)
+        {
+            if (Count >= length)
+            {
+                Dequeue();
+            }
+            base.Enqueue(item);
+        }
+
+        private readonly int length;
+    }
+}
