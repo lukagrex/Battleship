61e0a96490bbe44a763ac95cd893c9c1678a846e
Ivan Sokolović <isokolovic1@gmail.com>
2022-03-21 19:57:45 +0100

    Implement GetHorizondalPlacements method

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -27,15 +27,46 @@ namespace Vsite.Battleship.Model
 
         public IEnumerable<Square> squares 
         {
-            get
-            {
-                squares.Cast<Square>().Where(s => s != null);
-            }
+            get{ squares.Cast<Square>().Where(s => s != null); }
         }
 
         public IEnumerable<Square> GetAvailablePlacements(int length)
         {
-            throw new NotImplementedException();
+            return GetHorizontalPlacements(length).Concat(GetVerticalPlacements(length))
+        }
+
+        public IEnumerable<Square> GetHorizontalPlacements(int length)
+        {
+            List<SquareSequence> result = new List<SquareSequence> ();
+
+            for(int i = 0; i < Rows; i++)
+            {
+                int squaresInSequence = 0;
+                for(var j = 0; j < Columns; j++)
+                {
+                    if(squares[i, j] != null)
+                    {
+                        squaresInSequence++;
+                        if(squaresInSequence >= length) 
+                        { 
+                            List<Square> s = new List<Square>();
+
+                            for(int k = j - length; k <= j; ++k)
+                            {
+                                s.Add(new Square(i, k));
+                            }
+                            result.Add(s);
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        public IEnumerable<Square> GetVerticalPlacements(int length)
+        {
+            List<SquareSequence> result = new List<SquareSequence>();
+            return result;
         }
 
         public readonly int Rows;
