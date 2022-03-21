e7a5bb37beb5efca8fd22beaf2780811925c3da4
Andrea Vrljic <AVrljic@dspace.hr>
2022-03-21 19:44:19 +0100

    Create Grid class

---
+++ Model/Grid.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Vsite.Battleship.Model
+{
+    using SquareSequence = IEnumerable<Square>;
+    public class Grid
+    {
+        public readonly int Rows;
+        public readonly int Columns;
+
+        private Square[,] squares;
+
+        public Grid(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            squares = new Square[Rows, Columns];
+            for (int r = 0; r < Rows; ++r)
+            {
+                for (int c = 0; c < Columns; ++c)
+                {
+                    squares[r, c] = new Square(r, c);
+                }
+            }
+
+        }
+
+        public IEnumerable<Square> Squares
+        {
+            get { return squares.Cast<Square>().Where(s => s != null); }
+        }
+
+
+        public IEnumerable<SquareSequence> GetAvailablePlacements(int length)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
--- Model/Square.cs
+++ Model/Square.cs
@@ -1,6 +1,8 @@
-﻿namespace Vsite.Battleship.Model
+﻿using System;
+
+namespace Vsite.Battleship.Model
 {
-    public class Square
+    public class Square : IEquatable<Square>
     {
         public readonly int Row;
         public readonly int Column;
@@ -11,5 +13,36 @@
             Row = row;
             Column = column;
         }
+
+        public bool Equals(Square other)
+        {
+            return other != null && Row == other.Row && Column == other.Column;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 240067226;
+            hashCode = hashCode * -1521134295 + Row.GetHashCode();
+            hashCode = hashCode * -1521134295 + Column.GetHashCode();
+            return hashCode;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            return Equals((Square)obj);
+        }
     }
+
+
+
+
 }
