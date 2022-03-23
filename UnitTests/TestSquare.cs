0f41d22cc095cd85c3deaa215acd73ef2a5b4263
Damir Stojanović <DStojanovic@dspace.hr>
2022-03-23 07:35:00 +0100

    Change project properties

--- UnitTests/TestSquare.cs
+++ UnitTests/TestSquare.cs
@@ -1,8 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Model;
+using Vsite.Battleship.Model;
 using System;
 
-namespace UnitTests
+namespace Vsite.Battleship
 {
     [TestClass]
     public class TestSquare
