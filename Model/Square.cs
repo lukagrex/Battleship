84fb1b8aa286114aa9ff1ccbfee152f78c972752
Damir Stojanović <DStojanovic@dspace.hr>
2022-03-23 09:24:33 +0100

    Create Grid class

--- Model/Square.cs
+++ Model/Square.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Vsite.Battleship.Model
 {
-    public class Square
+    public class Square : IEquatable<Square>
     {
         public Square(int row, int column)
         {
@@ -17,6 +17,29 @@ namespace Vsite.Battleship.Model
         public readonly int Row;
         public readonly int Column;
 
+        public bool Equals(Square other)
+        {
+            return other != null && Row == other.Row && Column == other.Column;
+        }
 
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            if (GetType() != obj.GetType())
+                return false;
+            return Equals((Square)obj);
+        
+        }
+
+        public override int GetHashCode()
+        {
+            //int hashCode = 240067226;
+            //hashCode = hashCode * -1521134295 + Row.GetHashCode();
+            //hashCode = hashCode * -1521134295 + Column.GetHashCode();
+            //return hashCode;
+
+            return Row ^ Column;
+        }
     }
 }
