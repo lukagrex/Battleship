d12d5eac478cd98b87358064d1e86a73c4cdf086
Kata Pogacic <KPogacic@dspace.hr>
2022-03-28 20:29:52 +0200

    Add Fleet

---
+++ Model/Fleet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Vsite.BattleShip.Model
+{
+    public class Fleet
+    {
+        public void CreateShip(IEnumerable<Square> squares)
+        {
+            //pozovi konstruktor broda i dodaj ga u kolekciju
+        }
+
+        IEnumerable<Ship> Ships
+        {
+            get { return ships; }
+        }
+        private List<Ship> ships = new List<Ship>();
+    }
+}
---
+++ UnitTests/TestFleet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Vsite.BattleShip
+{
+    [TestClass]
+    public class TestFleet
+    {
+        [TestMethod]
+        public void CreateShipAndAddsNewShipToFleet()
+        {
+            Fleet fleet = new Fleet();
+            fleet.CreateShip(new List<Square>(){
+                new Square(1, 1), new Square(1, 2), new Square(1, 3)});
+            Assert.AreEqual(1, fleet.Ships.Count);
+        }
+    }
+}
+}
