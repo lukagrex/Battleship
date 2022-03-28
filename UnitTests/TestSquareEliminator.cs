99898579435c14ec1486384e5874d54c7becaef6
Ivan Sokolović <isokolovic1@gmail.com>
2022-03-28 20:40:51 +0200

    Create squareEliminator class, to be fixed

---
+++ UnitTests/TestSquareEliminator.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Vsite.Battleship
+{
+    [TestClass]
+    public class TestSquareEliminator
+    {
+        [TestMethod]
+        public void ToEliminateReturns18SquaresForShipIn4x3_4x6()
+        {
+            var eliminator = new TestSquareEliminator(10, 10);
+            eliminator.
+        }
+    }
+}
