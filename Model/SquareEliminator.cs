3242ff93284bc0c39b1ea645d2ad2a399b49e5e3
Antonio Brkic <ABrkic@dspace.hr>
2022-03-28 20:40:48 +0200

    Create SquareEliminator class

---
+++ Model/SquareEliminator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vsite.Battleship.Model
+{
+    public class SquareEliminator
+    {
+        public SquareEliminator(int rows, int columns)
+        {
+            this.rows = rows;
+            this.columns = columns;
+        }
+
+        public IEnumerable<Square> ToEliminate(IEnumerable<Square> shipSquares)
+        {
+            throw new NotImplementedException();
+        }
+
+        private readonly int rows;
+        private readonly int columns;
+    }
+}
