3a079fd720ec50cd3d1a4550ac1293d233fd2fa6
Robert Haramincic <RHaramincic@dspace.hr>
2022-03-28 20:41:02 +0200

    Create SquareEliminator class

---
+++ Model/SquareEliminator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.BattleShip.Model
+{
+    public class SquareEliminator
+    {
+        private readonly int rows;
+        private readonly int columns;
+
+        public SquareEliminator(int rows, int columns)
+        {
+            this.rows = rows;
+            this.columns = columns;
+        }
+
+        public IEnumerable<Square> ToEliminate(IEnumerable<Square> shipsSquares)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTests/TestSquareEliminator.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Vsite.BattleShip.Model;
+
+namespace Vsite.BattleShip.UnitTests
+{
+    [TestClass]
+    public class TestSquareEliminator
+    {
+        [TestMethod]
+        public void ToEliminateReturn18SquaresForShipIn4x3_4x6()
+        {
+            var eliminator = new SquareEliminator(10, 10);
+            var toEliminate = eliminator.ToEliminate(new List<Square>
+            {
+                new Square(4, 4),
+                new Square(4, 4),
+                new Square(4, 5),
+                new Square(4, 6)
+            });
+
+            Assert.AreEqual(18, toEliminate.Count());
+        }
+    }
+}
