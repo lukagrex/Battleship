0ae57612c374de9b30d5f1778f5c99479e990cb7
Ivan Sokolović <isokolovic1@gmail.com>
2022-03-28 20:29:41 +0200

    Add Fleet

---
+++ Model/Fleet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Battleship.Model
+{
+    public class Fleet
+    {
+        public void CreateShip(IEnumerable<Square> squares)
+        {
+
+        }
+
+        public IEnumerable<Ship> Ships { get { return Ships} } 
+
+    }
+}
---
+++ UnitTests/TestFleet.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vsite.Battleship.Model;
+using System.Linq;
+
+namespace Vsite.Battleship
+{
+    [TestClass]
+    public class TestFleet
+    {
+        [TestMethod]
+        public void CreateShipAddsNewShipToFleet()
+        {
+            Fleet fleet = new Fleet();
+
+            fleet.CreateShip(new Square(1, 1), new Square(1, 2), new Square(1, 3));
+
+        }
+    }
+}
