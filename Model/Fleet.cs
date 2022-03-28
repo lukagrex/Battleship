e7892bbf9057c8a905bc2f303b1edb5f278361db
Marina Letica <MLetica@dspace.hr>
2022-03-28 20:29:44 +0200

    Add Fleet

---
+++ Model/Fleet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Battleship.Model
+{
+    public class Fleet
+    {
+        public void CreateShip(IEnumerable<Square> squares)
+        {
+            //TODO
+        }
+
+
+
+        public IEnumerable<Ship> Ships
+        {
+            get => ships;
+        }
+        private List<Ship> ships = new List<Ship>();
+    }
+}
