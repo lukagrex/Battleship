9ac7c7d98db286019bc3225b3febc0a323f57590
Robert Haramincic <RHaramincic@dspace.hr>
2022-03-28 20:29:50 +0200

    Add Fleet class

---
+++ Model/Fleet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.BattleShip.Model
+{
+    public class Fleet
+    {
+        private List<Ship> ships = new List<Ship>();
+
+        public IEnumerable<Ship> Ships
+        {
+            get { return ships; }
+        }
+
+        public Fleet()
+        {
+            
+        }
+
+        public void CreateShip(IEnumerable<Square> squares)
+        {
+
+        }
+    }
+}
