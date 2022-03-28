d101ae9e8285371838d459372c57e9eaac718fd7
Marina Letica <MLetica@dspace.hr>
2022-03-28 20:40:47 +0200

    Create SquareEliminator class

---
+++ UnitTests/TestSquareEliminator.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Vsite.Battleship.Model;
+
+namespace Vsite.Battleship
+{
+    [TestClass]
+    public class TestSquareEliminator
+    {
+        [TestMethod]
+        public void ToEliminateReturns18SquaresForShipInSquares4x3_4x6()
+        {
+            var eliminitaor = new SquareEliminator(10, 10);
+            var toEliminate = eliminitaor.ToEliminate(new List<Square>
+                {new Square(4, 3), new Square(4, 4), new Square(4, 5), new Square(4, 6)});
+            Assert.AreEqual(18, toEliminate.Count());
+        }
+    }
+}
