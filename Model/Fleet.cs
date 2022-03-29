394664ff5bdabd44c69cf9613c295123bd818a92
Andrea Vrljic <AVrljic@dspace.hr>
2022-03-29 19:42:40 +0200

    Add fleet

---
+++ Model/Fleet.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Vsite.Battleship.Model
+{
+    public class Fleet
+    {
+        public void CreateShip(IEnumerable<Square> squares)
+        {
+            //TODO CREATE SHIP
+        }
+
+        public IEnumerable<Ship> Ships
+        {
+            get { return ships; }
+        }
+        //TODO: konstruktor broda dodat u kolekciju shipova
+        private List<Ship> ships = new List<Ship>();
+    }
+}
