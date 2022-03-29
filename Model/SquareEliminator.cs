dfc186d3ce2761ae5eb707ad33c0568baefe070f
Antonio Brkic <ABrkic@dspace.hr>
2022-03-29 12:48:29 +0200

    DZ Implement ToEliminate method and add border test cases

--- Model/SquareEliminator.cs
+++ Model/SquareEliminator.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Vsite.Battleship.Model
 {
@@ -13,7 +13,26 @@ namespace Vsite.Battleship.Model
 
         public IEnumerable<Square> ToEliminate(IEnumerable<Square> shipSquares)
         {
-            throw new NotImplementedException();
+            var toEliminateSquares = new List<Square>();
+            var ii = new List<int>() { -1, 0, 1 };
+            foreach (var shipSquare in shipSquares)
+            {
+                foreach (var i in ii)
+                {
+                    foreach (var j in ii)
+                    {
+                        // Do not eliminate squares out of boundaries
+                        if ((shipSquare.Row + i) > rows || (shipSquare.Row + i <= 0) ||
+                            (shipSquare.Column + j) > columns || (shipSquare.Column + j) <= 0)
+                        {
+                            continue;
+                        }
+                        toEliminateSquares.Add(new Square(shipSquare.Row + i, shipSquare.Column + j));
+                    }
+                }
+            }
+
+            return toEliminateSquares.Distinct();
         }
 
         private readonly int rows;
--- UnitTests/TestSquareEliminator.cs
+++ UnitTests/TestSquareEliminator.cs
@@ -16,5 +16,23 @@ namespace Vsite.Battleship
                 {new Square(4, 3), new Square(4, 4), new Square(4, 5), new Square(4, 6)});
             Assert.AreEqual(18, toEliminate.Count());
         }
+
+        [TestMethod]
+        public void ToEliminateReturns12SquaresForShipInSquares1x3_1x6()
+        {
+            var eliminator = new SquareEliminator(10, 10);
+            var toEliminate = eliminator.ToEliminate(new List<Square>
+                {new Square(1, 3), new Square(1, 4), new Square(1, 5), new Square(1, 6)});
+            Assert.AreEqual(12, toEliminate.Count());
+        }
+
+        [TestMethod]
+        public void ToEliminateReturns6SquaresForShipInSquares10x9_10x10()
+        {
+            var eliminator = new SquareEliminator(10, 10);
+            var toEliminate = eliminator.ToEliminate(new List<Square>
+                {new Square(10, 9), new Square(10, 10)});
+            Assert.AreEqual(6, toEliminate.Count());
+        }
     }
 }
