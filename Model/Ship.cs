fa33c45ba53ae9321088ebc89175366f4fcf4ec7
Andrea Vrljic <AVrljic@dspace.hr>
2022-03-29 19:31:31 +0200

    Add Ship class

---
+++ Model/Ship.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Vsite.Battleship.Model
+{
+    public class Ship
+    {
+        public readonly IEnumerable<Square> Squares;
+        public Ship(IEnumerable<Square> squares)
+        {
+            this.Squares = squares;
+        }
+    }
+}
