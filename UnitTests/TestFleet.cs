73090c7e1c2b743a0a69cb90c972266ef4f8c92e
Vicko Cagalj <vickocagalj@gmail.com>
2022-04-04 19:35:25 +0200

    Fix TestFleet unit test

--- UnitTests/TestFleet.cs
+++ UnitTests/TestFleet.cs
@@ -15,9 +15,9 @@ namespace Vsite.Battleship
             Fleet fleet = new Fleet();
             fleet.CreateShip(new List<Square> { new Square(1, 1), new Square(1, 2), new Square(1, 3) });
             Assert.AreEqual(1, fleet.Ships.Count());
-            
+
             fleet.CreateShip(new List<Square> { new Square(1, 8), new Square(2, 8), new Square(3, 8) });
-            Assert.AreEqual(1, fleet.Ships.Count());
+            Assert.AreEqual(2, fleet.Ships.Count());
         }
     }
 }
