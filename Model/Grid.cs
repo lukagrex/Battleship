2fe3b136b5bfc411908decafdfcf826a31400d4f
Pavle Pavicic <ppavicic@dspace.hr>
2022-04-04 20:24:29 +0200

    Added Shipwright class and corresponding unit test.

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -36,7 +36,7 @@ namespace Vsite.Battleship.Model
         public IEnumerable<SquareSequence> GetAvailablePlacements(int length)
         {
             return GetPlacements(length, new LoopIndex(Rows, Columns), (i, j) => squares[i, j])
-                .Concat(GetPlacements(length, new LoopIndex(Columns, Rows), (i, j) => squares[j, i]));
+                .Concat(GetPlacements(length, new LoopIndex(Columns, Rows), (i, j) => squares[j, i])).Where(pl => pl.Count() > 0);
         }
 
         class LoopIndex
