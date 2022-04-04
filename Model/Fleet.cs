63e7430445b1410d25359c1c38590348a2878c93
Andrea <vrlji.andrea@gmail.com>
2022-04-04 18:31:00 +0100

    Implement SquareEliminator

--- Model/Fleet.cs
+++ Model/Fleet.cs
@@ -4,7 +4,7 @@ namespace Vsite.Battleship.Model
 {
     public class Fleet
     {
-        //TODO: konstruktor broda dodat u kolekciju shipova
+   
         private List<Ship> ships = new List<Ship>();
 
         public void CreateShip(IEnumerable<Square> squares)
