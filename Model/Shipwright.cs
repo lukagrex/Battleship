7624f240358db532cac8aa1f94444850cbe9940c
Antonio Brkic <ABrkic@dspace.hr>
2022-04-04 20:24:11 +0200

    Implement Shipwright class

---
+++ Model/Shipwright.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Vsite.Battleship.Model
+{
+    public class Shipwright
+    {
+
+        private Grid grid;
+        private IEnumerable<int> shipLengths;
+        private Random random = new Random();
+        private SquareEliminator squareEliminator;
+
+        public Shipwright(int rows, int columns, IEnumerable<int> shipLengths)
+        {
+            grid = new Grid(rows, columns);
+            squareEliminator = new SquareEliminator(rows, columns);
+        }
+
+
+        public Fleet CreateFleet()
+        {
+            var fleet = new Fleet();
+
+            foreach (var shipLength in shipLengths)
+            {
+                var availablePlacements = grid.GetAvailablePlacements(shipLength);
+                int index = random.Next(availablePlacements.Count());
+                var selectedPlacement = availablePlacements.ElementAt(index);
+                fleet.CreateShip(selectedPlacement);
+                var toEliminate = squareEliminator.ToEliminate(selectedPlacement);
+                foreach (var square in toEliminate)
+                {
+                    grid.EliminateSquare(square.Row, square.Column);
+                }
+            }
+
+            return fleet;
+        }
+
+    }
+}
