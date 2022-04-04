7885e9098c360014b30774d20361d060a83ca47e
Andrea <vrlji.andrea@gmail.com>
2022-04-04 07:58:48 +0100

    Clean

--- Model/SquareEliminator.cs
+++ Model/SquareEliminator.cs
@@ -8,23 +8,6 @@ namespace Vsite.Battleship.Model
         private readonly int rows;
         private readonly int columns;
 
-        //public IEnumerable<Square> ToEliminate(IEnumerable<Square> shipSquares)
-        //{
-        //    HashSet<Square> eliminated = new HashSet<Square>();
-
-        //    foreach (var square in shipSquares)
-        //    {
-        //        for (int i = Math.Max(square.Row - 1, 0); i <= Math.Min(square.Row + 1, rows - 1); ++i)
-        //        {
-        //            for (int j = Math.Max(square.Column - 1, 0); j <= Math.Min(square.Column + 1, columns - 1); ++j)
-        //            {
-        //                eliminated.Add(new Square(i, j));
-        //            }
-        //        }
-        //    }
-        //    return eliminated;
-        //}
-
         public IEnumerable<Square> ToEliminate(IEnumerable<Square> shipSquares)
         {
             HashSet<Square> eliminated = new HashSet<Square>();
