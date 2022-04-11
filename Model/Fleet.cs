4551e83bb55b435e63b97912fbaf0495b21360f2
Ivan Spanja <IvanSp@dspace.hr>
2022-04-11 23:03:10 +0200

    Implement Shoot method for Ship

--- Model/Fleet.cs
+++ Model/Fleet.cs
@@ -6,11 +6,31 @@ using System.Threading.Tasks;
 
 namespace Vsite.Battleship.Model
 {
+    public enum HitResult
+    {
+        Missed,
+        Hit,
+        Sunken
+    }
     public class Fleet
     {
         public void CreateShip(IEnumerable<Square> squares)
         {
             ships.Add(new Ship(squares));
+        }
+
+        public HitResult Shoot(int row, int column)
+        {
+            foreach (var ship in ships)
+            {
+                var hitResult = ship.Shoot(row, column);
+                if (hitResult != HitResult.Missed)
+                {
+                    return hitResult;
+                }
+            }
+
+            return HitResult.Missed;
         }
 
         public IEnumerable<Ship> Ships
--- Model/Ship.cs
+++ Model/Ship.cs
@@ -13,6 +13,30 @@ namespace Vsite.Battleship.Model
             this.Squares = squares;
         }
 
+        public HitResult Shoot(int row, int column)
+        {
+            if (!Squares.Contains(new Square(row, column)))
+            {
+                return HitResult.Missed;
+            }
+
+            var hitSquare = Squares.First(s => s.Row == row && s.Column == column);
+            hitSquare.ChangeState(SquareState.Hit);
+
+            int squaresHit = Squares.Count(s => s.SquareState == SquareState.Hit || s.SquareState == SquareState.Sunken);
+            if (squaresHit == Squares.Count())
+            {
+                foreach (var square in Squares)
+                {
+                    square.ChangeState(SquareState.Sunken);
+                }
+
+                return HitResult.Sunken;
+            }
+
+            return HitResult.Hit;
+        }
+
         public readonly IEnumerable<Square> Squares;
     }
 }
