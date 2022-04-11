051717cd7f000ae7f7f7a803b2598ea695c6712c
AndjelaJ <AJuric@dspace.hr>
2022-04-11 20:24:08 +0200

    Add Shoot function

--- Model/Ship.cs
+++ Model/Ship.cs
@@ -13,6 +13,29 @@ namespace Vsite.Battleship.Model
             Squares = squares;
         }
 
+        public HitResult Shoot(int row, int column)
+        {
+            if (!Squares.Contains(new Square(row, column)))
+                return HitResult.Missed;
+
+            var hitSquare = Squares.First(s => s.Row == row && s.Column == column);
+            hitSquare.ChangeState(SquareState.Hit);
+
+            int squaresHit = Squares.Count(s => s.SquareState != SquareState.Missed || s.SquareState != SquareState.Initial);
+
+            if (squaresHit == Squares.Count())
+            {
+                foreach (var square in Squares)
+                {
+                    square.ChangeState(SquareState.Sunken);
+                }
+
+                return HitResult.Sunken;
+            }
+
+            return HitResult.Hit;
+        }
+
         public readonly IEnumerable<Square> Squares;
     }
 }
