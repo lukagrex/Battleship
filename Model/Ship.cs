0c10f80b97b562a982fc9de74ab18e2195c06bb8
Robert Haramincic <RHaramincic@dspace.hr>
2022-04-11 23:03:10 +0200

    HomeWork - finish implementation of the method Shoot in Ship class. Add new test cases for Shoot the ship in TestShip class.

--- Model/Ship.cs
+++ Model/Ship.cs
@@ -19,25 +19,36 @@ namespace Vsite.BattleShip.Model
         {
             if (!Squares.Contains(new Square(row, column)))
             {
+                // The square doesn't belong to any ship
                 return HitResult.Missed;
             }
 
             var hitSquare = Squares.First(s => s.Row == row && s.Column == column);
-            hitSquare.ChangeState(SquareState.Hit);
-
-            int squaresHit = Squares.Count(s => s.SquareState == SquareState.Hit);
-            
-            if (squaresHit == Squares.Count())
+            if (hitSquare.SquareState == SquareState.Initial)
             {
-                foreach (var square in Squares)
-                {
-                    square.ChangeState(SquareState.Sunken);
-                }
-
-                return HitResult.Sunken;
+                hitSquare.ChangeState(SquareState.Hit);
+            }
+            else
+            {
+                // This square is already Missed or Hit or Sunken
+                return (HitResult)((int)hitSquare.SquareState - 1);
             }
 
-            return HitResult.Hit;
+            // Check how many ship squares are hit
+            var squaresHit = Squares.Count(s => s.SquareState == SquareState.Hit);
+            if (squaresHit != Squares.Count())
+            {
+                // The ship square is Hit
+                return HitResult.Hit;
+            }
+            
+            // All squares of the ship are hit --> Sunken
+            foreach (var square in Squares)
+            {
+                square.ChangeState(SquareState.Sunken);
+            }
+
+            return HitResult.Sunken;
         }
     }
 }
--- UnitTests/TestShip.cs
+++ UnitTests/TestShip.cs
@@ -36,8 +36,11 @@ namespace Vsite.BattleShip.UnitTests
             });
 
             var result = ship.Shoot(2, 5);
-
             Assert.AreEqual(HitResult.Missed, result);
+
+            // The State of this square is already Missed
+            var result2 = ship.Shoot(2, 5);
+            Assert.AreEqual(HitResult.Missed, result2);
         }
 
         [TestMethod]
@@ -52,25 +55,13 @@ namespace Vsite.BattleShip.UnitTests
 
             var result = ship.Shoot(1, 2);
             Assert.AreEqual(HitResult.Hit, result);
-        }
 
-        [TestMethod]
-        public void ShootReturnsHitForSecondAttemptIfTargetSquareIsInShip()
-        {
-            var ship = new Ship(new List<Square>
-            {
-                new Square(1, 1),
-                new Square(1, 2),
-                new Square(1, 3)
-            });
-
-            var result = ship.Shoot(1, 2);
-            Assert.AreEqual(HitResult.Hit, result);
-
+            // The state of this square is already Hit
             var result2 = ship.Shoot(1, 2);
             Assert.AreEqual(HitResult.Hit, result2);
         }
 
+        [TestMethod]
         public void ShootReturnsSunkenIfTargetSquareIsInShip()
         {
             var ship = new Ship(new List<Square>
@@ -88,28 +79,10 @@ namespace Vsite.BattleShip.UnitTests
 
             var result2 = ship.Shoot(1, 3);
             Assert.AreEqual(HitResult.Sunken, result2);
-        }
 
-        public void ShootReturnsSunkenIfTargetSquareIsInShipIfHitAgain()
-        {
-            var ship = new Ship(new List<Square>
-            {
-                new Square(1, 1),
-                new Square(1, 2),
-                new Square(1, 3)
-            });
-
-            var result = ship.Shoot(1, 2);
-            Assert.AreEqual(HitResult.Hit, result);
-
-            var result1 = ship.Shoot(1, 1);
-            Assert.AreEqual(HitResult.Hit, result1);
-
-            var result2 = ship.Shoot(1, 3);
-            Assert.AreEqual(HitResult.Sunken, result2);
-
+            // The state of this square is already Sunken
             var result3 = ship.Shoot(1, 2);
-            Assert.AreEqual(HitResult.Sunken, result);
+            Assert.AreEqual(HitResult.Sunken, result3);
         }
     }
 }
