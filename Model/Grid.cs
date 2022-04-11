b868649e299157a8ed14a8a10b0656dece8b05b7
Pavle Pavicic <ppavicic@dspace.hr>
2022-04-11 19:33:21 +0200

    Added ToArray() inside Grid.cs.

--- Model/Grid.cs
+++ Model/Grid.cs
@@ -76,7 +76,7 @@ namespace Vsite.Battleship.Model
                         queue.Enqueue(squareSelect(o, i));
                         if (queue.Count >= length)
                         {
-                            result.Add(queue);
+                            result.Add(queue.ToArray());
                         }
                     }
                     else
