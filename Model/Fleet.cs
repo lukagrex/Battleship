158b525d1fe83b71f3e05488aee00a49e66796fa
Marina Letica <MLetica@dspace.hr>
2022-04-13 05:44:46 +0200

    Added some enums

--- Model/Fleet.cs
+++ Model/Fleet.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace Vsite.Battleship.Model
 {
+    public enum HitResult
+    {
+        Missed,
+        Hit,
+        Sunken
+    }
+
     public class Fleet
     {
         public void CreateShip(IEnumerable<Square> squares)
@@ -13,7 +20,17 @@ namespace Vsite.Battleship.Model
             this.ships.Add(new Ship(squares));
         }
 
+        public HitResult Shoot(int row, int column)
+        {
+            foreach (var ship in ships)
+            {
+                var hitResult = ship.Shoot(row, column);
+                if (hitResult != HitResult.Missed)
+                    return hitResult;
+            }
 
+            return HitResult.Missed;
+        }
 
         public IEnumerable<Ship> Ships
         {
--- Model/Ship.cs
+++ Model/Ship.cs
@@ -13,6 +13,24 @@ namespace Vsite.Battleship.Model
             this.Squares = squares;
         }
 
+        public HitResult Shoot(int row, int column)
+        {
+            if (!Squares.Contains(new Square(row, column)))
+                return HitResult.Missed;
+            var hitSquares = Squares.First(s => s.Row == row && s.Column == column);
+            hitSquares.ChangeState(SquareState.Hit);
+            int squaresHit = Squares.Count(s => s.SquareState != SquareState.Missed);
+
+            if (squaresHit == Squares.Count())
+            {
+                foreach (var squares in Squares)
+                    squares.ChangeState(SquareState.Sunken);
+                return HitResult.Sunken;
+            }
+
+            return HitResult.Hit;
+        }
+
         public readonly IEnumerable<Square> Squares;
     }
 }
