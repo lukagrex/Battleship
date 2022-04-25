fdeb44a3d7617025a76cd94d7246b65313e36dca
Ivan Sokolovic <ISokolovic@dspace.hr>
2022-04-25 19:23:56 +0200

    Add Gunnery and tests

---
+++ Model/Gunnery.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Battleship.Model
+{
+    public enum ShootingTactics
+    {
+        Random, 
+        Surrounding,
+        Inline
+    }
+
+    public class Gunnery
+    {
+        public Gunnery(int rows, int columns, IEnumerable<int> shipLengths)
+        {
+
+        }
+
+        public void ProcessHitResult(HitResult hitResult)
+        {
+
+        }
+
+        private ShootingTactics currentTactics = Model.ShootingTactics.Random;
+
+        public ShootingTactics ShootingTactics
+        {
+            get
+            {
+                return currentTactics;
+            }
+        }
+
+    }
+}
