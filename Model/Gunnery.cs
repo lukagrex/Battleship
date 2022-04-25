95cc61f1c09f28ec10115954ccc6ece86b75c09e
Antonio Brkic <ABrkic@dspace.hr>
2022-04-25 19:24:38 +0200

    Implement tests for Gunnery class

---
+++ Model/Gunnery.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Vsite.Battleship.Model
+{
+    public enum ShootingTactics
+    {
+        Random,
+        Surrounding,
+        Inline
+    }
+
+    public class Gunnery
+    {
+        private ShootingTactics currentTactics = ShootingTactics.Random;
+
+        public Gunnery(int rows, int columns, IEnumerable<int> shipLengths)
+        {
+            //TODO create evidenceGrid
+        }
+
+        public void ProcessHitResult(HitResult hitResult)
+        {
+            //TODO Implement to change currentTactics
+        }
+
+        public ShootingTactics ShootingTactics => currentTactics;
+    }
+}
