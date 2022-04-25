da5b26c550ce7d5f3b9b3de8cec4377a3300471f
Branko Rados <BRados@dspace.hr>
2022-04-25 19:20:41 +0200

    Add Gunnary class and UnitTest for changing it state

---
+++ Model/Gunnery.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Battleship.Model
+{
+    public enum ShootingTactics
+    {
+        Random,
+        Surrounding,
+        Inline
+    }
+    public class Gunnery
+    {
+        public Gunnery(int row, int columns, IEnumerable<int> shipLengths)
+        {
+            
+        }
+
+        private ShootingTactics currentTactics = ShootingTactics.Random;
+
+        public ShootingTactics ShootingTactics
+        {
+            get
+            {
+                return currentTactics;
+            }
+        }
+
+        public void ProcessHitResult(HitResult hitResult)
+        {
+            if (hitResult == HitResult.Hit && currentTactics == ShootingTactics.Random)
+            {
+                currentTactics = ShootingTactics.Surrounding;
+            }
+            else if (hitResult == HitResult.Hit && currentTactics == ShootingTactics.Surrounding)
+
+            {
+                currentTactics = ShootingTactics.Inline;
+            }
+            else if (hitResult == HitResult.Sunken)
+            {
+                currentTactics = ShootingTactics.Random;
+            }
+        }
+    }
+}
---
+++ UnitTests/TestGunnary.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using Vsite.Battleship.Model;
+
+namespace Vsite.Battleship
+{
+    [TestClass]
+    public class TestGunnary
+    {
+        [TestMethod]
+        public void InitiallyGunneryState()
+        {
+            var gunnary = new Gunnery(10, 10, new List<int>() {5, 3 });
+
+            Assert.AreEqual(ShootingTactics.Random, gunnary.ShootingTactics);
+        }
+
+        [TestMethod]
+        public void HitResultMissedGunnaryStateIsRandomIfPreviousStateIsRandom()
+        {
+            var gunnary = new Gunnery(10, 10, new List<int>() { 5, 3 });
+
+            gunnary.ProcessHitResult(HitResult.Missed);
+
+            Assert.AreEqual(ShootingTactics.Random, gunnary.ShootingTactics);
+        }
+
+        [TestMethod]
+        public void HitResultHitGunnaryStateIsSourroundingIfPreviousStateIsRandom()
+        {
+            var gunnary = new Gunnery(10, 10, new List<int>() { 5, 3 });
+
+            gunnary.ProcessHitResult(HitResult.Hit);
+
+            Assert.AreEqual(ShootingTactics.Surrounding, gunnary.ShootingTactics);
+        }
+
+        [TestMethod]
+        public void HitResultMissedGunnaryStateIsSourroundingIfPreviousStateIsSourrounding()
+        {
+            var gunnary = new Gunnery(10, 10, new List<int>() { 5, 3 });
+
+            gunnary.ProcessHitResult(HitResult.Hit);
+            gunnary.ProcessHitResult(HitResult.Missed);
+
+            Assert.AreEqual(ShootingTactics.Surrounding, gunnary.ShootingTactics);
+        }
+
+        [TestMethod]
+        public void HitResultHitGunnaryStateIsInlineIfPreviousStateIsSourrounding()
+        {
+            var gunnary = new Gunnery(10, 10, new List<int>() { 5, 3 });
+
+            gunnary.ProcessHitResult(HitResult.Hit);
+            gunnary.ProcessHitResult(HitResult.Hit);
+
+            Assert.AreEqual(ShootingTactics.Inline, gunnary.ShootingTactics);
+        }
+
+        [TestMethod]
+        public void HitResultMissedGunnaryStateIsInlineIfPreviousStateIsInline()
+        {
+            var gunnary = new Gunnery(10, 10, new List<int>() { 5, 3 });
+
+            gunnary.ProcessHitResult(HitResult.Hit);
+            gunnary.ProcessHitResult(HitResult.Hit);
+            gunnary.ProcessHitResult(HitResult.Missed);
+
+
+            Assert.AreEqual(ShootingTactics.Inline, gunnary.ShootingTactics);
+        }
+
+        [TestMethod]
+        public void HitResultSunkenGunnaryStateIsRandomIfPreviousStateIsInline()
+        {
+            var gunnary = new Gunnery(10, 10, new List<int>() { 5, 3 });
+
+            gunnary.ProcessHitResult(HitResult.Hit);
+            gunnary.ProcessHitResult(HitResult.Hit);
+            gunnary.ProcessHitResult(HitResult.Sunken);
+
+            Assert.AreEqual(ShootingTactics.Random, gunnary.ShootingTactics);
+        }
+    }
+}
