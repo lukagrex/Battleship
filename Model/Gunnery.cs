b790eefa2d7519af26137f8ffc410dc8eab7226e
Julijan Sribar <julijan.sribar@gmail.com>
2022-04-25 19:29:13 +0200

    Define shooting tactics

---
+++ Model/Gunnery.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Battleship.Model
+{
+    public enum ShootingTactics
+    {
+        Random,
+        Surrounding,
+        Inline
+    }
+
+    public class Gunnery
+    {
+        public Gunnery(int rows, int columns, IEnumerable<int> shipLengths)
+        { 
+        }
+
+        public void ProcessHitResult(HitResult hitResult)
+        { 
+        }
+
+        private ShootingTactics currentTactics = ShootingTactics.Random;
+        public ShootingTactics ShootingTactics
+        {
+            get { return currentTactics; }
+        }
+    }
+}
