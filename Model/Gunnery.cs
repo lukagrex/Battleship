7924819fc958c84acabc2225b1204e1d0e21cf97
Pavle Pavicic <ppavicic@dspace.hr>
2022-04-25 19:24:09 +0200

    Added Gunnery class; Added tests for Gunnery.

---
+++ Model/Gunnery.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Battleship.Model
+{
+    public enum ShootingTactics { 
+        Random,
+        Surrounding,
+        Inline
+    }
+    public class Gunnery
+    {
+        public Gunnery(int row, int columns, IEnumerable<int> shipLengths)
+        {
+            
+        }
+        public void ProcessHitResult(HitResult hitResult)
+        { 
+            // For Homework
+        }
+        private ShootingTactics currentTactics = ShootingTactics.Random;
+        public ShootingTactics ShootingTactics
+        {
+            get { return currentTactics; }
+        }
+    }
+}
