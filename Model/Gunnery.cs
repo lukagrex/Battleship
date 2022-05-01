0000651aaefd4345591fa7f5cf83626f5a1d6360
Ivan Spanja <IvanSp@dspace.hr>
2022-05-01 20:39:09 +0200

    Add Evidence grid

--- Model/Gunnery.cs
+++ Model/Gunnery.cs
@@ -16,15 +16,36 @@ namespace Vsite.Battleship.Model
     public class Gunnery
     {
         private ShootingTactics currentTactics = ShootingTactics.Random;
+        private EvidenceGrid evidenceGrid;
 
         public Gunnery(int rows, int columns, IEnumerable<int> shipLengths)
         {
-            //TODO create evidenceGrid
+            evidenceGrid = new EvidenceGrid(rows, columns);
         }
 
         public void ProcessHitResult(HitResult hitResult)
         {
-            //TODO Implement to change currentTactics
+            switch (hitResult)
+            {
+                case HitResult.Missed:
+                    return;
+                case HitResult.Hit:
+                    switch (ShootingTactics)
+                    {
+                        case ShootingTactics.Random:
+                            currentTactics = ShootingTactics.Surrounding;
+                            return;
+                        case ShootingTactics.Surrounding:
+                            currentTactics = ShootingTactics.Inline;
+                            return;
+                        case ShootingTactics.Inline:
+                            return;
+                    }
+                    return;
+                case HitResult.Sunken:
+                    currentTactics = ShootingTactics.Random;
+                    return;
+            }
         }
 
         public ShootingTactics ShootingTactics => currentTactics;
