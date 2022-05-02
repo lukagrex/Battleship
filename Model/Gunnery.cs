55ab531c4f5f60bbb9944ee013eb7bc23d62d8d2
AndjelaJ <AJuric@dspace.hr>
2022-05-02 19:09:37 +0200

    Add squares hit to Gunnery

--- Model/Gunnery.cs
+++ Model/Gunnery.cs
@@ -24,10 +24,12 @@ namespace Vsite.Battleship.Model
 
         private Grid monitoringGrid;
         private List<Square> squaresHit = new List<Square>();
+        private Square lastTarget;
 
         public Square NextTarget()
         {
-            return targetSelector.NextTarget();
+            lastTarget = targetSelector.NextTarget();
+            return lastTarget;
         }
 
         public void ProcessHitResult(HitResult hitResult)
@@ -37,10 +39,12 @@ namespace Vsite.Battleship.Model
                 case HitResult.Missed:
                     return;
                 case HitResult.Hit:
+                    squaresHit.Add(lastTarget);
                     if (currentTactics == ShootingTactics.Inline)
                         return;
                     break;
                 case HitResult.Sunken:
+                    squaresHit.Clear();
                     break;
                 default:
                     Debug.Assert(false);
