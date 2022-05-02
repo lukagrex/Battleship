0a3fb22f4052eaca76e17c4996d53961c75f755f
Pavle Pavicic <ppavicic@dspace.hr>
2022-05-02 19:02:27 +0200

    Added ShootingTactics classes; Added INextTarget interface.

--- Model/Gunnery.cs
+++ Model/Gunnery.cs
@@ -13,23 +13,37 @@ namespace Vsite.Battleship.Model
     }
     public class Gunnery
     {
-        public Gunnery(int row, int columns, IEnumerable<int> shipLengths)
+        public Gunnery(int rows, int columns, IEnumerable<int> shipLengths)
         {
-            
+            this.monitoringGrid = new Grid(rows, columns);
+            this.currentTactics = ShootingTactics.Random;
+            this.targetSelector = new RandomShooting(monitoringGrid);
         }
+
+        private Grid monitoringGrid;
+        private List<Square> squaresHit = new List<Square>();
+
+        public Square NextTarget()
+        {
+            return targetSelector.NextTarget();
+        }
+
         public void ProcessHitResult(HitResult hitResult)
         {
             if (hitResult == HitResult.Hit && currentTactics == ShootingTactics.Random)
             {
                 currentTactics = ShootingTactics.Surrounding;
+                targetSelector = new SurroundingShooting(monitoringGrid, squaresHit.First());
             }
             else if (hitResult == HitResult.Hit && currentTactics == ShootingTactics.Surrounding)
             {
                 currentTactics = ShootingTactics.Inline;
+                targetSelector = new InlineShooting(monitoringGrid, squaresHit);
             }
             else if (hitResult == HitResult.Sunken)
             {
                 currentTactics = ShootingTactics.Random;
+                targetSelector = new RandomShooting(monitoringGrid);
             }
         }
         private ShootingTactics currentTactics = ShootingTactics.Random;
@@ -37,5 +51,7 @@ namespace Vsite.Battleship.Model
         {
             get { return currentTactics; }
         }
+
+        private INextTarget targetSelector;
     }
 }
---
+++ Model/SurroundingShooting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Battleship.Model
+{
+    public class SurroundingShooting : INextTarget
+    {
+        public SurroundingShooting(Grid grid, Square firstSquareHit)
+        {
+            this.grid = grid;
+            this.firstSquareHit = firstSquareHit;
+        }
+
+        private Grid grid;
+        private readonly Square firstSquareHit;
+        public Square NextTarget()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
