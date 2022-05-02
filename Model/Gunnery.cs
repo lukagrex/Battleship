5337fab41858096219075a751336ca800c59b67c
Pavle Pavicic <ppavicic@dspace.hr>
2022-05-02 19:09:43 +0200

    Add squares hit to Gunnery.

--- Model/Gunnery.cs
+++ Model/Gunnery.cs
@@ -22,10 +22,12 @@ namespace Vsite.Battleship.Model
 
         private Grid monitoringGrid;
         private List<Square> squaresHit = new List<Square>();
+        private Square lastTarget;
 
         public Square NextTarget()
         {
-            return targetSelector.NextTarget();
+            lastTarget = targetSelector.NextTarget();
+            return lastTarget;
         }
 
         public void ProcessHitResult(HitResult hitResult)
