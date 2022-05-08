2046a79f0ebb7e395d75595b02f9399ee0ce7284
Vicko Cagalj <vickocagalj@gmail.com>
2022-05-08 19:58:30 +0200

    Add shooting tactics

--- Model/Gunnery.cs
+++ Model/Gunnery.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlTypes;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,8 +19,18 @@ namespace Vsite.Battleship.Model
     {
         public Gunnery(int rows, int columns, IEnumerable<int> shipLengths)
         {
-
+            monitoringGrid = new Grid(rows, columns);
+            ChangeToRandomTactics();
         }
+
+        private Grid monitoringGrid;
+        private List<Square> squaresHit = new List<Square>();
+
+        public Square NextTarget()
+        {
+            return targetSelector.NextTarget();
+        }
+
         public void ProcessHitResult(HitResult hitResult)
         {
             switch (hitResult)
@@ -30,20 +41,43 @@ namespace Vsite.Battleship.Model
                     switch (ShootingTactics)
                     {
                         case ShootingTactics.Random:
-                            currentTactics = ShootingTactics.Surrounding;
+                            ChangeToSurroundingTactics();
                             return;
                         case ShootingTactics.Surrounding:
-                            currentTactics = ShootingTactics.Inline;
+                            ChangeToInlineTactics();
                             return;
                         case ShootingTactics.Inline:
                             return;
+                        default:
+                            Debug.Assert(false);
+                            break;
                     }
                     return;
                 case HitResult.Sunken:
-                    currentTactics = ShootingTactics.Random;
+                    ChangeToRandomTactics();
                     return;
             }
         }
+
+        private void ChangeToRandomTactics()
+        {
+            currentTactics = ShootingTactics.Random;
+            targetSelector = new RandomShooting(monitoringGrid);
+
+        }
+
+        private void ChangeToSurroundingTactics()
+        {
+            currentTactics = ShootingTactics.Surrounding;
+            targetSelector = new SurroundingShooting(monitoringGrid, squaresHit.First());
+        }
+
+        private void ChangeToInlineTactics()
+        {
+            currentTactics = ShootingTactics.Inline;
+            targetSelector = new InlineShooting(monitoringGrid, squaresHit);
+        }
+
 
         private ShootingTactics currentTactics = ShootingTactics.Random;
 
@@ -51,5 +85,7 @@ namespace Vsite.Battleship.Model
         {
             get { return currentTactics; }
         }
+
+        private INextTarget targetSelector;
     }
 }
---
+++ Model/SurroundingShooting.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Battleship.Model
+{
+    public class SurroundingShooting : INextTarget
+    {
+        public SurroundingShooting(Grid grid, Square firstSquareHit)
+        {
+            this.grid = grid;
+            this.firstSquareHit = firstSquareHit;
+        }
+
+        private readonly Square firstSquareHit;
+        private Grid grid;
+
+        public Square NextTarget()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
