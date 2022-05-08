7ea732947931c11f7ae00a817b4a5be9f1e8a5c8
Vicko Cagalj <vickocagalj@gmail.com>
2022-05-08 15:08:10 +0200

    Implement Shoot Method for Ship and create the corresponding tests

--- UnitTests/TestShip.cs
+++ UnitTests/TestShip.cs
@@ -18,5 +18,54 @@ namespace Vsite.Battleship
             CollectionAssert.Contains(ship.Squares.ToArray(), new Square(1, 2));
             CollectionAssert.Contains(ship.Squares.ToArray(), new Square(1, 3));
         }
+
+        [TestMethod]
+        public void ShootReturnsMissedIfTargetSquareIsNotInShip()
+        {
+            var ship = new Ship(new List<Square> { new Square(1, 1), new Square(1, 2), new Square(1, 3) });
+            var result = ship.Shoot(2, 5);
+            Assert.AreEqual(HitResult.Missed, result);
+        }
+
+        [TestMethod]
+        public void ShootReturnsHitIfTargetSquareIsInShip()
+        {
+            var ship = new Ship(new List<Square> { new Square(1, 1), new Square(1, 2), new Square(1, 3) });
+            var result = ship.Shoot(1, 2);
+            Assert.AreEqual(HitResult.Hit, result);
+        }
+
+        [TestMethod]
+        public void ShootReturnsHitForSecondAttemptIfTargetSquareIsInShip()
+        {
+            var ship = new Ship(new List<Square> { new Square(1, 1), new Square(1, 2), new Square(1, 3) });
+            var result = ship.Shoot(1, 2);
+            Assert.AreEqual(HitResult.Hit, result);
+            result = ship.Shoot(1, 2);
+            Assert.AreEqual(HitResult.Hit, result);
+        }
+
+        [TestMethod]
+        public void ShootReturnsSunkenForTheLastShipSquare()
+        {
+            var ship = new Ship(new List<Square> { new Square(1, 1), new Square(1, 2), new Square(1, 3) });
+            ship.Shoot(1, 2);
+            ship.Shoot(1, 3);
+            var result = ship.Shoot(1, 1);
+            Assert.AreEqual(HitResult.Sunken, result);
+        }
+
+        [TestMethod]
+        public void ShootReturnsSunkenForTheLastShipSquareIfItIsHitAgain()
+        {
+            var ship = new Ship(new List<Square> { new Square(1, 1), new Square(1, 2), new Square(1, 3) });
+            ship.Shoot(1, 2);
+            ship.Shoot(1, 3);
+            ship.Shoot(1, 1);
+            var result = ship.Shoot(1, 3);
+            Assert.AreEqual(HitResult.Sunken, result);
+        }
+
+
     }
 }
