60a4d265a9bcd5eb67fb645f5a721343635026f3
Pavle Pavicic <ppavicic@dspace.hr>
2022-05-09 16:49:01 +0200

    Implemented NextTarget() in SurroundingShooting.

--- Model/SurroundingShooting.cs
+++ Model/SurroundingShooting.cs
@@ -20,7 +20,15 @@ namespace Vsite.Battleship.Model
         private readonly int shipLength;
         public Square NextTarget()
         {
-            throw new NotImplementedException();
+            int row = firstSquareHit.Row;
+            int column = firstSquareHit.Column;
+            var candidates = grid.Squares.Where(s => s.SquareState == SquareState.Initial
+                                                     && ((s.Row == row && (s.Column == column - 1 || s.Column == column + 1)) 
+                                                     || (s.Column == column && (s.Row == row - 1 || s.Row == row + 1))));
+
+            if (!candidates.Any()) return null;
+
+            return candidates.FirstOrDefault();
         }
     }
 }
