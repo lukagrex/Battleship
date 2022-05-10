e472360c5a0d72ff96a7a33f3f9300faf7456522
Antonio Brkic <ABrkic@dspace.hr>
2022-05-10 13:38:51 +0200

    Implement InlineShooting class

--- Model/EnemyGrid.cs
+++ Model/EnemyGrid.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 
 namespace Vsite.Battleship.Model
 {
+    using SquareSequence = IEnumerable<Square>;
+
     public enum Direction
     {
         Up,
@@ -22,9 +24,42 @@ namespace Vsite.Battleship.Model
             squares[row, column].ChangeState(newState);
         }
 
-        public IEnumerable<Square> GetAvailableSquares(int row, int column, Direction direction)
+        public SquareSequence GetAvailableSquares(int row, int column, Direction direction)
         {
-            throw new NotImplementedException();
+            var deltaRow = 0;
+            var deltaColumn = 0;
+            var counter = 0;
+            switch (direction)
+            {
+                case Direction.Left:
+                    deltaColumn = -1;
+                    counter = column;
+                    break;
+                case Direction.Up:
+                    deltaRow = -1;
+                    counter = row;
+                    break;
+                case Direction.Right:
+                    deltaColumn = +1;
+                    counter = Columns - column - 1;
+                    break;
+                case Direction.Down:
+                    deltaRow = +1;
+                    counter = Rows - row - 1;
+                    break;
+            }
+            var result = new List<Square>();
+            for (int i = 0; i < counter; ++i)
+            {
+                row += deltaRow;
+                column += deltaColumn;
+                if (squares[row, column].SquareState != SquareState.Initial)
+                {
+                    break;
+                }
+                result.Add(new Square(row, column));
+            }
+            return result;
         }
 
         public void RecordTheShooting()
--- Model/InlineShooting.cs
+++ Model/InlineShooting.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Vsite.Battleship.Model
 {
@@ -7,16 +8,43 @@ namespace Vsite.Battleship.Model
     {
         private EnemyGrid enemyGrid;
         private List<Square> squaresHit;
+        private Direction direction;
 
         public InlineShooting(EnemyGrid enemyGrid, List<Square> squaresAlreadyHit, int shipLength)
         {
+            if (squaresAlreadyHit.Count < 2)
+            {
+                throw new ArgumentException("Inline shooting requires at least 2 squares already hit!");
+            }
             this.enemyGrid = enemyGrid;
             this.squaresHit = squaresAlreadyHit;
+            this.direction = squaresAlreadyHit[0].Row == squaresAlreadyHit[1].Row ? Direction.Left : Direction.Up;
         }
 
         public Square NextTarget()
         {
-            throw new NotImplementedException();
+            if (direction == Direction.Left || direction == Direction.Right)
+            {
+                var row = squaresHit[0].Row;
+                var leftColumn = squaresHit.Min(p => p.Column);
+                var rightColumn = squaresHit.Max(p => p.Column);
+
+                var availableSquaresLeft = enemyGrid.GetAvailableSquares(row, leftColumn, Direction.Left);
+                var availableSquaresRight = enemyGrid.GetAvailableSquares(row, rightColumn, Direction.Right);
+
+                return availableSquaresRight.Count() > availableSquaresLeft.Count() ? availableSquaresRight.First() : availableSquaresLeft.First();
+            }
+            else
+            {
+                var column = squaresHit[0].Column;
+                var upRow = squaresHit.Min(p => p.Row);
+                var downRow = squaresHit.Max(p => p.Row);
+
+                var availableSquaresUp = enemyGrid.GetAvailableSquares(upRow, column, Direction.Up);
+                var availableSquaresDown = enemyGrid.GetAvailableSquares(downRow, column, Direction.Down);
+
+                return availableSquaresUp.Count() > availableSquaresDown.Count() ? availableSquaresUp.First() : availableSquaresDown.First();
+            }
         }
     }
 }
