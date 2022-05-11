5b4c38e312c9c71fd28cf139e1630e0aab2607bd
brxyzd <brxyzd@hotmail.com>
2022-05-11 20:29:04 +0200

    Prevent weakening of the SquareState in EnemyGrid

--- Model/EnemyGrid.cs
+++ Model/EnemyGrid.cs
@@ -20,7 +20,12 @@ namespace Vsite.Battleship.Model
         }
         public void ChangeSquareState(int row, int column, SquareState newState)
         {
-            //TODO: Prevent weakening square state (e.g. if square is sunken, it cannot be hit)
+            if (squares[row, column].SquareState == SquareState.Sunken || squares[row, column].SquareState == SquareState.Missed)
+            {
+                // Can not change Sunken and Missed states
+                return;
+            }
+            
             squares[row, column].ChangeState(newState);
         }
 
