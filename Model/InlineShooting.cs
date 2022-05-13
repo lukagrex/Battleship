38a821a3a0a1dea1e9277ca0caa7f272ef80cf31
AndjelaJ <AJuric@dspace.hr>
2022-05-13 17:14:43 +0200

    Implement NextTarget method for InlineShooting

--- Model/InlineShooting.cs
+++ Model/InlineShooting.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace Vsite.Battleship.Model
 {
+    public enum ShipPosition
+    {
+        Horizontal,
+        Vertical
+    }
     public class InlineShooting : INextTarget
     {
         public InlineShooting(EnemyGrid grid, List<Square> squaresAlreadyHit, int shipLength)
@@ -14,14 +19,47 @@ namespace Vsite.Battleship.Model
             this.squaresAlreadyHit = squaresAlreadyHit;
             this.shipLength = shipLength;
         }
-        
+
         private EnemyGrid grid;
         private List<Square> squaresAlreadyHit;
         private int shipLength;
 
         public Square NextTarget()
         {
-            throw new NotImplementedException();
+            IEnumerable<Square> candidatesFirst;
+            IEnumerable<Square> candidatesLast;
+
+            var shipPosition = getShipPosition();
+
+            if (shipPosition == ShipPosition.Horizontal)
+            {
+                int row = squaresAlreadyHit.FirstOrDefault().Row;
+                int firstColumn = squaresAlreadyHit.Min(x => x.Column);
+                int lastColumn = squaresAlreadyHit.Max(x => x.Column);
+                candidatesFirst = grid.GetAvailableSquares(row, firstColumn, Direction.Leftwards);
+                candidatesLast = grid.GetAvailableSquares(row, lastColumn, Direction.Rightwards);
+            }
+            else
+            {
+                int column = squaresAlreadyHit.FirstOrDefault().Column;
+                int firstRow = squaresAlreadyHit.Min(x => x.Row);
+                int lastRow = squaresAlreadyHit.Max(x => x.Row);
+
+                candidatesFirst = grid.GetAvailableSquares(firstRow, column, Direction.Upwards);
+                candidatesLast = grid.GetAvailableSquares(lastRow, column, Direction.Bottomwards);
+            }
+
+            return candidatesFirst.Count() > candidatesLast.Count() ? candidatesFirst.First() : candidatesLast.First();
+
+        }
+
+        ShipPosition getShipPosition()
+        {
+            if (squaresAlreadyHit.First().Row == squaresAlreadyHit.Last().Row &&
+                squaresAlreadyHit.First().Column != squaresAlreadyHit.Last().Column)
+                return ShipPosition.Horizontal;
+
+            return ShipPosition.Vertical;
         }
     }
 }
