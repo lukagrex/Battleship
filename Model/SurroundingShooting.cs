de7dea98e7721bb2307b1f4974e56b831cb91851
Julijan Sribar <julijan.sribar@gmail.com>
2022-05-15 11:16:54 +0200

    Implement SurroundingShooting

--- Model/SurroundingShooting.cs
+++ Model/SurroundingShooting.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace Vsite.Battleship.Model
 {
+    using SquareSequence = IEnumerable<Square>;
+
     public class SurroundingShooting : INextTarget
     {
         public SurroundingShooting(EnemyGrid grid, Square firstSquareHit, int shipLength)
@@ -17,11 +19,20 @@ namespace Vsite.Battleship.Model
 
         public Square NextTarget()
         {
-            throw new NotImplementedException();
+            List<SquareSequence> candidates = new List<SquareSequence>();
+            foreach (Direction direction in Enum.GetValues(typeof(Direction)))
+            {
+                var available = grid.GetAvailableSquares(firstSquareHit.Row, firstSquareHit.Column, direction);
+                if (available.Count() > 0)
+                    candidates.Add(available);
+            }
+            int index = random.Next(candidates.Count);
+            return candidates[index].First();
         }
 
         private readonly Square firstSquareHit;
         private EnemyGrid grid;
         private int shipLength;
+        private Random random = new Random();
     }
 }
