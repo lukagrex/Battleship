d60bc88fcd153cacf205b2a3d9ceb51dfa507ffe
Ivan Spanja <IvanSp@dspace.hr>
2022-05-15 21:36:11 +0200

    Update SurroundhSHooting and implement Inline Shooting

--- Model/EnemyGrid.cs
+++ Model/EnemyGrid.cs
@@ -6,21 +6,76 @@ using System.Threading.Tasks;
 
 namespace Vsite.Battleship.Model
 {
+    using SquareSequence = IEnumerable<Square>;
+
+    public enum Direction
+    {
+        Leftwards,
+        Upwards,
+        Rightwards,
+        Bottomwards
+    }
     public class EnemyGrid : Grid
     {
         public EnemyGrid(int rows, int columns) : base(rows, columns)
         {
-
         }
 
         public void ChangeSquareState(int row, int column, SquareState newState)
         {
-            squares[row, column].ChangeState(newState);
+            switch (squares[row, column].SquareState)
+            {
+                case SquareState.Initial:
+                    squares[row, column].ChangeState(newState);
+                    break;
+                case SquareState.Hit:
+                    if (newState == SquareState.Sunken)
+                        squares[row, column].ChangeState(newState);
+                    break;
+                case SquareState.Missed:
+                    break;
+                case SquareState.Sunken:
+                    break;
+            }
+
         }
 
-        public void GetAvailableSquares()
+        public SquareSequence GetAvailableSquares(int row, int column, Direction direction)
         {
-            throw new NotImplementedException();
+            int deltaRow = 0;
+            int deltaColumn = 0;
+            int counter = 0;
+            switch (direction)
+            {
+                case Direction.Leftwards:
+                    deltaColumn = -1;
+                    counter = column;
+                    break;
+                case Direction.Upwards:
+                    deltaRow = -1;
+                    counter = row;
+                    break;
+                case Direction.Rightwards:
+                    deltaColumn = +1;
+                    counter = Columns - column - 1;
+                    break;
+                case Direction.Bottomwards:
+                    deltaRow = +1;
+                    counter = Rows - row - 1;
+                    break;
+            }
+            List<Square> result = new List<Square>();
+            for (int i = 0; i < counter; ++i)
+            {
+                row += deltaRow;
+                column += deltaColumn;
+                if (squares[row, column].SquareState != SquareState.Initial)
+                {
+                    break;
+                }
+                result.Add(new Square(row, column));
+            }
+            return result;
         }
 
         protected override bool IsSquareAvailable(int i1, int i2, Func<int, int, Square> squareSelect)
--- Model/InlineShooting.cs
+++ Model/InlineShooting.cs
@@ -13,15 +13,57 @@ namespace Vsite.Battleship.Model
             this.grid = grid;
             this.squaresAlreadyHit = squaresAlreadyHit;
             this.shipLength = shipLength;
+
+            var isHorizontal = squaresAlreadyHit.Select(x => x.Row).Distinct().Count() == 1;
+
+            if (isHorizontal)
+            {
+                var first = squaresAlreadyHit.FirstOrDefault(x => x.Column == squaresAlreadyHit.Select(y => y.Column).Min());
+                var last = squaresAlreadyHit.FirstOrDefault(x => x.Column == squaresAlreadyHit.Select(y => y.Column).Max());
+
+                var leftCount = grid.GetAvailableSquares(first.Row, first.Column, Direction.Leftwards).Count();
+                var rightCount = grid.GetAvailableSquares(first.Row, first.Column, Direction.Rightwards).Count();
+
+                if (leftCount > 0)
+                    hitList.Add(Direction.Leftwards, new Square(first.Row, first.Column - 1));
+                numOfSquaresInDirection.Add(Direction.Leftwards, leftCount);
+
+                if (rightCount > 0)
+                    hitList.Add(Direction.Rightwards, new Square(last.Row, last.Column + 1));
+                numOfSquaresInDirection.Add(Direction.Rightwards, rightCount);
+            }
+            else
+            {
+                var first = squaresAlreadyHit.FirstOrDefault(x => x.Row == squaresAlreadyHit.Select(y => y.Row).Min());
+                var last = squaresAlreadyHit.FirstOrDefault(x => x.Row == squaresAlreadyHit.Select(y => y.Row).Max());
+
+                var upCount = grid.GetAvailableSquares(first.Row, first.Column, Direction.Upwards).Count();
+                var bottomCount = grid.GetAvailableSquares(first.Row, first.Column, Direction.Bottomwards).Count();
+
+                if (upCount > 0)
+                    hitList.Add(Direction.Upwards, new Square(first.Row - 1, first.Column));
+                numOfSquaresInDirection.Add(Direction.Upwards, upCount);
+
+                if (bottomCount > 0)
+                    hitList.Add(Direction.Bottomwards, new Square(last.Row + 1, last.Column));
+                numOfSquaresInDirection.Add(Direction.Bottomwards, bottomCount);
+            }
+
         }
 
         private EnemyGrid grid;
         private List<Square> squaresAlreadyHit;
         private int shipLength;
+        private Dictionary<Direction, Square> hitList = new Dictionary<Direction, Square>();
+        private Dictionary<Direction, int> numOfSquaresInDirection = new Dictionary<Direction, int>();
 
         public Square NextTarget()
         {
-            throw new NotImplementedException();
+            var bestDirectionCount = numOfSquaresInDirection.Values.Max();
+            var bestDirection = numOfSquaresInDirection.FirstOrDefault(x => x.Value == bestDirectionCount).Key;
+            hitList.TryGetValue(bestDirection, out var selectedSquare);
+            hitList.Remove(bestDirection);
+            return selectedSquare;
         }
     }
 }
