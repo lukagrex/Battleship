f26c39f10f54fe8709285fa574510860a0523766
Robert Haramincic <RHaramincic@dspace.hr>
2022-05-16 19:53:08 +0200

    Add TestSortedSquare

---
+++ Model/SortedSquares.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Vsite.BattleShip.Model
+{
+    public class SortedSquares : List<Square>
+    {
+        public SortedSquares()
+        {
+        }
+
+        public SortedSquares(IEnumerable<Square> collection) : base(collection)
+        {
+            this.SortSquares();
+        }
+
+        public new void Add(Square square)
+        {
+            base.Add(square);
+            this.SortSquares();
+        }
+
+        private void SortSquares()
+        {
+            Sort((sq1, sq2) => sq1.Column + sq1.Row - sq2.Row - sq2.Column);
+        }
+    }
+}
---
+++ UnitTests/TestSortSquares.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vsite.BattleShip.Model;
+
+namespace Vsite.BattleShip.UnitTests
+{
+    [TestClass]
+    public class TestSortSquares
+    {
+        [TestMethod]
+        public void ConstructorCreateCollectionOfSquaresSortedByColumnForHorizontallyPlacedSquares()
+        {
+            SortedSquares coll = new SortedSquares(new Square[] { new Square(3, 4), new Square(3, 3), new Square(3, 5) });
+            Assert.AreEqual(3, coll.Count);
+            Assert.AreEqual(new Square(3, 3), coll[0]);
+            Assert.AreEqual(new Square(3, 4), coll[1]);
+            Assert.AreEqual(new Square(3, 5), coll[2]);
+        }
+
+        [TestMethod]
+        public void ConstructorCreateCollectionOfSquaresSortedByColumnForVerticallyPlacedSquares()
+        {
+            SortedSquares coll = new SortedSquares(new Square[] { new Square(3, 4), new Square(2, 4), new Square(4, 4) });
+            Assert.AreEqual(3, coll.Count);
+            Assert.AreEqual(new Square(2, 4), coll[0]);
+            Assert.AreEqual(new Square(3, 4), coll[1]);
+            Assert.AreEqual(new Square(4, 4), coll[2]);
+        }
+
+        [TestMethod]
+        public void AfterAddMethodIsInvokedSquaresAreSortedByColumnsForHorizontallyPlacedSquares()
+        {
+            SortedSquares coll = new SortedSquares(new Square[] { new Square(3, 4), new Square(3, 5) });
+            Assert.AreEqual(2, coll.Count);
+            coll.Add(new Square(3, 3));
+            Assert.AreEqual(3, coll.Count);
+            Assert.AreEqual(new Square(3, 3), coll[0]);
+            Assert.AreEqual(new Square(3, 4), coll[1]);
+            Assert.AreEqual(new Square(3, 5), coll[2]);
+        }
+
+        [TestMethod]
+        public void AfterAddMethodIsInvokedSquaresAreSortedByColumnsForVerticallyPlacedSquares()
+        {
+            SortedSquares coll = new SortedSquares(new Square[] { new Square(3, 4), new Square(4, 4) });
+            Assert.AreEqual(2, coll.Count);
+            coll.Add(new Square(2, 4));
+            Assert.AreEqual(3, coll.Count);
+            Assert.AreEqual(new Square(2, 4), coll[0]);
+            Assert.AreEqual(new Square(3, 4), coll[1]);
+            Assert.AreEqual(new Square(4, 4), coll[2]);
+        }
+    }
+}
