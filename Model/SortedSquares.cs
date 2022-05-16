c662c2a987d8eee440b2ad729109ae0ee6015495
Luka Gregorić <endo145@gmail.com>
2022-05-16 19:53:45 +0200

    Add SortedSquares class

---
+++ Model/SortedSquares.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vsite.Battleship.Model
+{
+
+    public class SortedSquares : List<Square>
+    {
+        public SortedSquares()
+        {
+        }
+
+        public SortedSquares(IEnumerable<Square> collection) : base(collection)
+        {
+            SortSquares();
+        }
+
+        public new void Add (Square square)
+        {
+            base.Add(square);
+            SortSquares();
+        }
+
+        private void SortSquares()
+        {
+            Sort((sq1, sq2) => sq1.Column + sq1.Row - (sq2.Column + sq2.Row));
+        }
+    }
+}
