ac6ddf096e30d9af0b0d228eb2cea575a0e028c8
Luka Gregorić <endo145@gmail.com>
2022-06-25 14:39:28 +0200

    Add basic layout

--- GUI/Form1.Designer.cs
+++ GUI/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace GUI
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,63 @@ namespace GUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.playerRadioButton = new System.Windows.Forms.RadioButton();
+            this.computerRadioButton = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // playerRadioButton
+            // 
+            this.playerRadioButton.AutoSize = true;
+            this.playerRadioButton.Location = new System.Drawing.Point(22, 13);
+            this.playerRadioButton.Name = "playerRadioButton";
+            this.playerRadioButton.Size = new System.Drawing.Size(69, 21);
+            this.playerRadioButton.TabIndex = 0;
+            this.playerRadioButton.TabStop = true;
+            this.playerRadioButton.Text = "Player";
+            this.playerRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // computerRadioButton
+            // 
+            this.computerRadioButton.AutoSize = true;
+            this.computerRadioButton.Location = new System.Drawing.Point(22, 41);
+            this.computerRadioButton.Name = "computerRadioButton";
+            this.computerRadioButton.Size = new System.Drawing.Size(90, 21);
+            this.computerRadioButton.TabIndex = 1;
+            this.computerRadioButton.TabStop = true;
+            this.computerRadioButton.Text = "Computer";
+            this.computerRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(159, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1182, 553);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.computerRadioButton);
+            this.Controls.Add(this.playerRadioButton);
+            this.Name = "MainForm";
+            this.Text = "Battleship";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton playerRadioButton;
+        private System.Windows.Forms.RadioButton computerRadioButton;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- GUI/Form1.cs
+++ GUI/Form1.cs
@@ -7,14 +7,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Vsite.Battleship.Model;
 
 namespace GUI
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
+            grid1 = new Button[100, 100];
+            grid2 = new Button[100, 100];
             InitializeComponent();
+            InitailizeGrid(grid1, "p1", 0);
+            InitailizeGrid(grid2, "p2", 350);
+        }
+
+        public void InitailizeGrid(Button[,] grid, string name, int offset)
+        {
+            this.SuspendLayout();
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    grid[i, j] = new System.Windows.Forms.Button();
+                    grid[i, j].Location = new System.Drawing.Point(offset + 20 + j * 30, 70 + i * 30);
+                    grid[i, j].Name = name + "button" + i.ToString() + j.ToString();
+                    grid[i, j].Size = new System.Drawing.Size(30, 30);
+                    grid[i, j].TabIndex = 0;
+                    grid[i, j].UseVisualStyleBackColor = true;
+                    this.Controls.Add(grid[i, j]);
+                }
+            }
+
+            this.ResumeLayout(false);
+        }
+
+        private readonly Button[,] grid1;
+        private readonly Button[,] grid2;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FleetGrid fleetGrid = new FleetGrid(10, 10);
+            EnemyGrid enemyGrid = new EnemyGrid(10, 10);
+            if (playerRadioButton.Checked)
+            {
+                //
+            }
         }
     }
 }
